8084b1d7d22b31206179a0fc697316cfd3ea99a3
Riddikuluz <117537617+Riddikuluz@users.noreply.github.com>
2023-06-21 00:54:33 -0400

    ADD idToken

--- app.js
+++ app.js
@@ -4,17 +4,18 @@ const session = require('express-session');
 const path = require('path');
 const app = express();
 const jwt = require('jsonwebtoken');
-const PUERTO = process.env.puerto || 3000
 require('dotenv').config();
 require('./auth');
+const puerto = process.env.puertoServer || process.env.puerto
 
 function isLogIn(req, res, next) {
   req.user ? next() : res.sendStatus(401);
 }
 
-//app.use(passport.initialize());
+app.use(passport.initialize());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.urlencoded({ extended: false }));
 
 app.use(session({
   secret: process.env.clave,
@@ -24,52 +25,63 @@ app.use(session({
 }))
 
 app.use(passport.session());
-
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');});
 
-app.get('/auth/google', passport.authenticate('google',
- {scope: [ 'email'] }));
+  app.get('/auth/google',
+    passport.authenticate('google', {
+      session: false,
+      scope: ["profile", "email"],
+      accessType: "offline",
+      approvalPrompt: "force"
+    })
+  );
 
 app.get('/auth/google/callback',
   passport.authenticate('google',
     {
       successRedirect: '/auth/jwt',
-      failureRedirect: '/auth/google/failure',
+      failureRedirect: '/auth/failure'
     }
   ));
 
 app.get('/auth/jwt', isLogIn, (req, res) => {
   const dataUser =req.user;
   //console.log(dataUser.azp);
-  jwt.sign({dataUser},process.env.clave,{expiresIn: '24h'},function(err, token){
+  jwt.sign( {dataUser}, process.env.clave, {expiresIn: '24h'}, function(err, token){
     if (err) {
       res.sendStatus(500);
     } else {
-      res.redirect(`/token?token=${token}`);
+      res.redirect(`/auth/jwt/token?token=${token}`);
     }});
 });
 
-app.get('/token', isLogIn, function(req, res) {
+app.get('/auth/jwt/token', isLogIn, function(req, res) {
   const token = req.query.token;
-  res.send(`Token JWT: ${token}`);
+  jwt.verify( token, process.env.clave, function(err, token){
+    if (err) {
+      res.sendStatus(500);
+    } else {
+      res.send(`Token JWT: ${req.query.token}`);
+      //res.send(`DATA JWT: ${JSON.stringify(token)}`);
+    }});
 });
 
-app.get('/auth/logout', isLogIn, (req, res) => {
+app.get('/auth/logout', (req, res) => {
   req.session.destroy();
   res.send("Hasta luego.");
 });
 
-app.get('/auth/failure', isLogIn, (req, res) => {
+app.get('/auth/failure', (req, res) => {
   res.sendStatus(500);
   res.send('Crenciales no validas.');
 });
 
-app.listen(PUERTO, () => {
-  console.log(`Escuchando en el puerto ${PUERTO}`);
+app.listen(puerto, () => {
+  console.log(`Escuchando en el puerto ${puerto}`);
 });
 
 
-//npm install express dotenv express-session passport passport-google-oauth2 jsonwebtoken axios
+//npm install express dotenv express-session passport passport-google-oauth2 jsonwebtoken axios google-auth-library
 //http://localhost:3000
 //https://jwt.io/
--- auth.js
+++ auth.js
@@ -4,20 +4,35 @@ require('dotenv').config();
 const axios = require("axios");
 
 passport.use(new GoogleStrategy({
-    clientID:          process.env.GOOGLE_CLIENT_ID,
-    clientSecret:      process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL:       process.env.HTTP_DOMAIN,
+    clientID:          process.env.google_Client_Id,
+    clientSecret:      process.env.google_Client_Secret,
+    callbackURL:       process.env.http_Domain_Server ||process.env.http_Domain,
     passReqToCallback: true
   },
-  async function(request, accessToken, refreshToken, profile, done) {
+  async function(request, accessToken, refreshToken,idToken, profile, done){
 
-       const response = await axios.get(
-          `https://www.googleapis.com/oauth2/v3/tokeninfo?access_token=${accessToken}`
-        );
-        const data = response.data;
-        done(null, data);
+    try {
+      const response = await axios.get(
+        `https://oauth2.googleapis.com/tokeninfo?id_token=${idToken.id_token}`
+      );
+      const data = {
+        iss: response.data.iss,
+        azp: response.data.azp,
+        aud: response.data.aud,
+        sub: response.data.sub,
+        hd: response.data.hd,
+        email: response.data.email,
+        email_verified: response.data.email_verified,
+        at_hash: response.data.at_hash,
+        iat: response.data.iat,
+        exp: response.data.exp,
+      };
+      //console.log(data);
+      done(null, data);
+    } catch (error) {
+      done(error);
+    }
       }));
-
 passport.serializeUser(
   (user,done)=>{done(null, user);
 });
