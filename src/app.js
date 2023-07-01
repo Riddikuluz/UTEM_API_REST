b26cfb882eebad3e003d3017904fe5298cac59c3
Riddikuluz <117537617+Riddikuluz@users.noreply.github.com>
2023-06-30 21:04:34 -0400

    ADD swagger

--- src/app.js
+++ src/app.js
@@ -3,11 +3,14 @@ const session = require("express-session");
 const express = require("express");
 const passport = require("passport");
 const path = require("path");
+const YAML = require("yamljs");
+const swaggerUi = require("swagger-ui-express");
 
 const routes = require("./routes/googleToken");
 const puerto = process.env.puertoServer;
 const voter = require("./routes/voter");
 require("./services/oauth20");
+const swaggerDocument = YAML.load(path.join(__dirname, "./docs/swagger.yaml"));
 
 // Configuraciones y middlewares de Express
 const app = express();
@@ -16,9 +19,9 @@ app.use(express.json()); // Permite el análisis de datos JSON en las solicitude
 app.use(express.static(path.join(__dirname, "client"))); // Define el directorio estático para servir archivos estáticos
 app.use(express.urlencoded({ extended: false })); // Permite el análisis de datos de formulario en las solicitudes
 app.use(express.static("public")); // Define el directorio estático adicional para servir archivos estáticos
-// Configuración de la sesión
 app.use(
   session({
+    // Configuración de la sesión
     secret: process.env.clave, // Clave secreta para firmar las cookies de sesión
     resave: false, // No vuelve a guardar la sesión si no ha sido modificada
     saveUninitialized: false, // Guarda sesiones sin inicializar
@@ -30,8 +33,11 @@ app.use(passport.session()); // Permite el uso de sesiones de Passport
 // Rutas Google
 routes(app);
 
-// metodos voter
+// Metodos voter
 voter(app);
+
+// swagger
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Iniciar el servidor
 app.listen(puerto, () => {
